47425dd378f1328679b6d535192d86f5c2ee1dfe
malena kiryluk <malenakiryluk@gmail.com>
2022-10-26 22:11:01 -0300

    detail

--- src/Componentes/ItemaListaConteiner.js
+++ src/Componentes/ItemaListaConteiner.js
@@ -1,5 +1,4 @@
 import React, { useEffect , useState } from "react";
-import { ItemCount } from "./ItemCount"
 import "./ItemListConteiner.css"
 import { ItemList} from "./ItemList"
 import { useParams } from "react-router-dom";
@@ -9,15 +8,15 @@ const ItemListConteiner = (props) =>{
 
     const [productos, setProductos] = useState([]);
 
-    const { productId } = useParams();
+    const { id } = useParams();
 
   const URL_BASE = 'https://fakestoreapi.com/products?limit=30'
-  const URL_CAT = `${URL_BASE}/category/${productId} `
+  const URL_CAT = `${URL_BASE}/category/${id} `
 
   useEffect(() => {
     const getProductos = async () => {
       try {
-        const res = await fetch(URL_CAT);
+        const res = await fetch(id ? URL_BASE : URL_CAT);
         const data = await res.json();
         setProductos(data);
       } catch {
@@ -26,7 +25,7 @@ const ItemListConteiner = (props) =>{
     };
     getProductos();
 
-  }, [])
+  }, [id, URL_BASE, URL_CAT])
 
     
 
